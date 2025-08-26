c354059e9711f992862a6a11b5ee9548605b48ce
Vyomkesh Chaudhary <vyomkesh.iitb@gmail.com>
2025-08-27 01:01:33 +0530

    v2

--- tryHello/models.py
+++ tryHello/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    dob = models.DateField()
+    is_active = models.BooleanField(default=True)
+    # student_id = models.IntegerField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Enrollment(models.Model):
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enroll_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+class TestTable(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'abc_table'
+        ordering = ['name']
+
+
+
+
+
+
+# CharField -> small sized text
+# TextField -> largeText
+# IntegerFiedl -> int Values
+# FloatFiedl
--- tryHello/views.py
+++ tryHello/views.py
@@ -1,7 +1,19 @@
+import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import Student
 
 
 # Create your views here.
 def hello_world(request, name):
-    return HttpResponse("Hello :" + name)
+    # s1 = Student(name=name, age=10, dob=datetime.date.today())
+    # s1.save()  # Save to DB
+
+    student = Student.objects.get(name=name)
+    student.age = 15
+    student.save()
+    return HttpResponse("Hello your age is :" + str(student.age))
+
+
